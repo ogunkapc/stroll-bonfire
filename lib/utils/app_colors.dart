8799f6bbcec8b38193a22e9323c34b068441ef62
Archer <ogunkapc@gmail.com>
2024-12-23 11:11:22 +0100

    chore: add assets for project (images and icons)

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primaryColor = Color(0xFF8B88EF);
+  static const Color headerTextColor = Color(0xFFCCC8FF);
+  static const Color questionTextColor = Color(0xFFF5F5F5);
+  static const Color optionsTextColor = Color(0xFFC4C4C4);
+  static const Color optionsCardColor = Color(0xFF232A2E);
+}
---
+++ lib/utils/app_images.dart
@@ -0,0 +1,13 @@
+class AppImages {
+  AppImages._();
+
+  //! Images
+  static const String background = 'assets/images/background_image.png';
+  static const String joeyImage = 'assets/images/joey2.png';
+
+  //! Icons
+  static const String pokerCards = 'assets/icons/poker_cards.svg';
+  static const String flameIcon = 'assets/icons/flame_icon.svg';
+  static const String chatIcon = 'assets/icons/chat_icon.svg';
+  static const String userIcon = 'assets/icons/user_icon.svg';
+}
